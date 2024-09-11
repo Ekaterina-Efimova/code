0321ed2af21013c48cd91d597ba51dedcdd05fdc
Ekaterina-Efimova <140252677+Ekaterina-Efimova@users.noreply.github.com>
2024-09-11 08:59:25 +0300

    Delete group/c directory

--- group/c/group/cpp/containers/s21_containersplus.hpp
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-// #include "s21_array/s21_array.hpp"
-#include "s21_set/s21_multiset.hpp"
--- group/c/group/cpp/containers/tests/tests.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <gtest/gtest.h>
-
-#include <memory>
-
-#include "../s21_containers.hpp"
-
-using namespace s21;
